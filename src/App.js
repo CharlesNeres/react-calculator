b38cd25a92a913f028c2c6aa9fe73be2cf3be88e
Charles Júnior <charlesbritoneresjr@gmail.com>
2024-10-22 18:54:19 +0800

    add html structure to the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="visor"></div>
+      <div className="container-c">7</div>
+      <div className="container-c">8</div>
+      <div className="container-c">9</div>
+      <div className="container-c">-</div>
+      <div className="container-c">4</div>
+      <div className="container-c">5</div>
+      <div className="container-c">6</div>
+      <div className="container-c">*</div>
+      <div className="container-c">1</div>
+      <div className="container-c">2</div>
+      <div className="container-c">3</div>
+      <div className="container-c">/</div>
+      <div className="container-c">0</div>
+      <div className="container-c">.</div>
+      <div className="container-c">AC</div>
+      <div className="container-c">+</div>
+      <div className="container-c equal">=</div>
     </div>
   );
 }
