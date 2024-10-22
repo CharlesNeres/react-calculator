55aea7eb0d90ec5d7707eba11d8b49f07c5ff9ba
Charles Júnior <charlesbritoneresjr@gmail.com>
2024-10-22 18:58:17 +0800

    implemented add functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import { useState } from "react";
 import "./App.css";
 function App() {
+  const [result, setResult] = useState("0");
+
+  function addValue(e) {
+    setResult((result) =>
+      result === "0" ? e.target.textContent : result + e.target.textContent
+    );
+  }
+
   return (
     <div className="container">
-      <div className="visor"></div>
-      <div className="container-c">7</div>
-      <div className="container-c">8</div>
-      <div className="container-c">9</div>
-      <div className="container-c">-</div>
-      <div className="container-c">4</div>
-      <div className="container-c">5</div>
-      <div className="container-c">6</div>
-      <div className="container-c">*</div>
-      <div className="container-c">1</div>
-      <div className="container-c">2</div>
-      <div className="container-c">3</div>
-      <div className="container-c">/</div>
-      <div className="container-c">0</div>
-      <div className="container-c">.</div>
+      <div className="visor">{result}</div>
+      <div className="container-c" onClick={addValue}>
+        7
+      </div>
+      <div className="container-c" onClick={addValue}>
+        8
+      </div>
+      <div className="container-c" onClick={addValue}>
+        9
+      </div>
+      <div className="container-c" onClick={addValue}>
+        -
+      </div>
+      <div className="container-c" onClick={addValue}>
+        4
+      </div>
+      <div className="container-c" onClick={addValue}>
+        5
+      </div>
+      <div className="container-c" onClick={addValue}>
+        6
+      </div>
+      <div className="container-c" onClick={addValue}>
+        *
+      </div>
+      <div className="container-c" onClick={addValue}>
+        1
+      </div>
+      <div className="container-c" onClick={addValue}>
+        2
+      </div>
+      <div className="container-c" onClick={addValue}>
+        3
+      </div>
+      <div className="container-c" onClick={addValue}>
+        /
+      </div>
+      <div className="container-c" onClick={addValue}>
+        0
+      </div>
+      <div className="container-c" onClick={addValue}>
+        .
+      </div>
       <div className="container-c">AC</div>
-      <div className="container-c">+</div>
+      <div className="container-c" onClick={addValue}>
+        +
+      </div>
       <div className="container-c equal">=</div>
     </div>
   );
